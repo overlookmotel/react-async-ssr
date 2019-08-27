697be4b05a4e21f8dfaf091489cd35d0a13a7985
overlookmotel <theoverlookmotel@gmail.com>
2019-08-27 12:39:04 +0100

    Update dev dependencies [dev]

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
 		'@overlookmotel/eslint-config-jest',
 		'@overlookmotel/eslint-config-react'
 	],
+	parserOptions: {
+		sourceType: 'script'
+	},
 	rules: {
 		'react/no-multi-comp': ['off'],
 		'react/jsx-filename-extension': ['error', {extensions: ['.jsx', '.test.js']}]
